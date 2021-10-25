73eb76fbc70454af3386afe4cfc3a5ad0ed972e4
Nikita Koval <me@nikitakoval.com>
2021-10-25 20:06:59 +0600

    Change way to build and push/pull images

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -115,9 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/bstatic/'
+STATIC_ROOT = '/bstatic'
+MEDIA_URL = '/bmedia/'
+MEDIA_ROOT = '/bmedia'
--- build.sh
+++ build.sh
@@ -2,19 +2,16 @@
 
 set -xe
 
+bump2version --allow-dirty patch
+VERSION=`cat VERSION`
+
 echo "Build test images"
 docker-compose -f docker-compose.base.yml -f docker-compose.ci.yml build
 
 echo "Run backend tests"
-docker-compose -f docker-compose.base.yml -f docker-compose.ci.yml run --rm backend pytest --junitxml=.test-results/be-report.xml
+VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.ci.yml run --rm backend pytest --junitxml=.test-results/be-report.xml
 echo "Run frontend tests"
-docker-compose -f docker-compose.base.yml -f docker-compose.ci.yml run --rm frontend npm run test:ci
+VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.ci.yml run --rm frontend-test npm run test:ci
 
-bump2version --allow-dirty patch
-
-VERSION=`cat VERSION`
-
-echo "Build images"
-VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml build
 echo "Pushing images"
-VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml push
+VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml push backend frontend
