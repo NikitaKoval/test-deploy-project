28ff60a59cf7d11ef4264b95c40f9f9fbc3ceccc
Nikita Koval <me@nikitakoval.com>
2021-10-25 17:01:05 +0600

    Change way using ssh key from secrets

--- build.sh
+++ build.sh
@@ -19,9 +19,8 @@ VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.p
 echo "Pushing images"
 VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml push
 
-echo ${GIT_SSH} > git_ssh
 chmod 600 git_ssh
-export GIT_SSH_COMMAND='ssh -i git_ssh'
+export GIT_SSH_COMMAND='ssh -i ${GIT_SSH}'
 git config user.name "Jenkins"
 git config user.email jenkins@localhost
 git push origin refs/remotes/origin/master
