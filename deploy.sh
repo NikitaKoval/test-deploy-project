13130ebc7b9dbbe783823c5530b3f1fe6920c6e7
Nikita Koval <me@nikitakoval.com>
2021-10-26 12:24:23 +0600

    Fix quotes issue

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ ln -sf ${ENV_PATH} .env.prod
 eval `ssh-agent -s`
 ssh-add -k ${SSH_KEY_PATH}
 ssh-keyscan -H ${DEPLOY_HOST} >> ~/.ssh/known_hosts
-export DOCKER_HOST='ssh://${SSH_USER}@${DEPLOY_HOST}'
-VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml pull -q
+export DOCKER_HOST="ssh://${SSH_USER}@${DEPLOY_HOST}"
 
+VERSION=${VERSION} docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml pull -q
 scp docker-compose.base.yml docker-compose.prod.yml .env.prod ${SSH_USER}@${DEPLOY_HOST}:~
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SSH_USER}@${DEPLOY_HOST} /bin/bash <<EOF
